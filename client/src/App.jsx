e050b0fdc425a81e22c15b613e71f360d498962d
BN2323 <Bunnaroth.tha@student.cadt.edu.kh>
2025-07-04 14:33:15 +0700

    update background style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import CreatureLab from "./pages/CreatureLab";
 export default function App() {
   return (
     <BrowserRouter>
+      <div className="absolute top-[-100px] left-[-100px] w-[300px] h-[300px] bg-[#0036a8] rounded-full -z-1 blur-[3px]"></div>
+      <div className="absolute bottom-[-100px] right-[-100px] w-[400px] h-[400px] bg-[#004fc9] rounded-full -z-1 blur-[3px]"></div>
       <nav>
         <Link to="/">Home</Link> |{" "}
         <Link to="/lesson">Tutorial</Link> |{" "}
